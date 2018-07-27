44d06fe0c4d6f6e0835ab315a1b21395af0c3490
sadhubrahmanandareddy <41730749+sadhubrahmanandareddy@users.noreply.github.com>
2018-07-27 09:53:00 +0530

    Update check_positive_or_nehgative.java

--- check_positive_or_nehgative.java
+++ check_positive_or_nehgative.java
@@ -1,10 +1,6 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
@@ -12,12 +8,15 @@ class Ideone
 		int a;
 		Scanner s=new Scanner(System.in);
 		a=s.nextInt();
-		if(a>0)
+		if(a>0){
 		  System.out.println("Positive");
-		else if(a>0)
+		}
+		else if(a>0){
 		   System.out.println("Negative");
-		else
+		}
+		else{
 	              System.out.println("Zero");
+		}
 
 
 	}
