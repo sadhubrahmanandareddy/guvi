4522b85e75edb6c19f54a540c7416fd71876dff9
sadhubrahmanandareddy <41730749+sadhubrahmanandareddy@users.noreply.github.com>
2018-07-27 11:49:27 +0530

    Create alphabet_or_not.java

---
+++ alphabet_or_not.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		 char c=s.next().charAt(0);//read a character variable just like integer variable
+		 if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+		 System.out.println("Alphabet");
+		 else
+		  System.out.println("No");
+	}
+}
