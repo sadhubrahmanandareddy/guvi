8dfb8d26445d27b12f9c0347a026140da53984dd
sadhubrahmanandareddy <41730749+sadhubrahmanandareddy@users.noreply.github.com>
2018-07-27 10:19:20 +0530

    Update positive_or_negative.java

--- positive_or_negative.java
+++ positive_or_negative.java
@@ -10,15 +10,18 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		int a;
-		Scanner s=new Scanner(System.in);
-		a=s.nextInt();
-		if(a>0){
-		  System.out.println("Positive");
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		if(a>0)
+		{
+		   System.out.println("Positive");
 		}
-		else if(a<0){
-		   System.out.println("Negative");
+		else if(a<0)
+		{
+		    System.out.println("Negative");
 		}
-		else{
+		else
+		{
 	              System.out.println("Zero");
 		}
 
