c321f3a5778938f6c95d7806c2d2a5de708054ae
sadhubrahmanandareddy <41730749+sadhubrahmanandareddy@users.noreply.github.com>
2018-07-31 14:37:40 +0530

    Update prime_number_or_not.java

--- prime_number_or_not.java
+++ prime_number_or_not.java
@@ -10,15 +10,14 @@ class Ideone
 		 int n, i, flag = 0;
 		 Scanner sc=new Scanner(System.in);
 		 n=sc.nextInt();
-		 for(i=2;i<=n/2;i++)
+		 for(i=1;i<=n;i++)
 		 {
 		 	if(n%i==0)
 		 	{
-		 	flag=1;
-		 	break;
+		 	flag+=1;
 		 	}
 		 }
-		 if(flag==0)
+		 if(flag==2)
 		 System.out.println("yes");
 		 else
 		 System.out.println("no");
