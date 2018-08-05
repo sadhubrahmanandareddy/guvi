279e3a742edd47258f2ce04066bd7f9f31c8946f
sadhubrahmanandareddy <41730749+sadhubrahmanandareddy@users.noreply.github.com>
2018-08-05 11:52:29 +0530

    Create print_elements_with_index.java

---
+++ print_elements_with_index.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.lang.Math;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{        
+		int i,n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int a[]=new int[10];
+		for(i=0;i<n;i++)
+		{
+		a[i]=s.nextInt();
+		System.out.println(a[i]+" "+i);
+		}
+		
+		
+	
+	}
+}
