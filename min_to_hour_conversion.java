ce5ce7e1ab8a635ca17ad75182c15c7d260e3adc
sadhubrahmanandareddy <41730749+sadhubrahmanandareddy@users.noreply.github.com>
2018-08-05 10:49:53 +0530

    Update min_to_hour_conversion.java

--- min_to_hour_conversion.java
+++ min_to_hour_conversion.java
@@ -1,28 +1,24 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner s=new Scanner(System.in);
-		int hours,min;
-		min=s.nextInt();
-		if(min<60)
+		int h,m;
+		m=s.nextInt();
+		if(m<60)
 		{
-			hours=0;
-	      System.out.print(hours+" "+min);
+			h=0;
+	      System.out.print(h+" "+m);
 	      
 		}
 		else
 		{
-			hours=min/60;
-			min=min%60;
-			System.out.print(hours+" "+min);
+			h=m/60;
+			m=m%60;
+			System.out.print(h+" "+m);
 		}
 		
 	}
