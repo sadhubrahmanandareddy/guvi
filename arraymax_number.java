42d73fc9d83c8755cb79332d0f52de152dae10d5
sadhubrahmanandareddy <41730749+sadhubrahmanandareddy@users.noreply.github.com>
2018-08-06 22:11:40 +0530

    Update arraymax_number.java

--- arraymax_number.java
+++ arraymax_number.java
@@ -1,3 +1,5 @@
+/* package whatever; // don't place package name! */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -7,7 +9,7 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-      Scanner s=new Scanner(System.in);
+		 Scanner s=new Scanner(System.in);
       int array[]=new int[10];
       int size,i;
       size=s.nextInt();
@@ -26,6 +28,10 @@ class Ideone
          	}
          }
          System.out.println(max);
+	}
+	else
+	{
+		
 	}
 	}
 }
