08ef098c09306cba9732eee13917f8fd2b02f8dd
sadhubrahmanandareddy <41730749+sadhubrahmanandareddy@users.noreply.github.com>
2018-08-08 14:00:55 +0530

    Update swap.java

--- swap.java
+++ swap.java
@@ -1,5 +1,3 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -10,15 +8,15 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	   Scanner s=new Scanner(System.in);
-	   int n1,n2,temp;
-	   n1=s.nextInt();
-	   n2=s.nextInt();
-	   if(n1<=100000&&n2<=100000)
+	   int num1,num2,temp;
+	   num1=s.nextInt();
+	   num2=s.nextInt();
+	   if(num1<=100000&&num2<=100000)
 	   {
-	   	 temp=n1;
-	   	 n1=n2;
-	   	 n2=temp;
-	   	 System.out.print(n1+" "+n2);
+	   	 temp=num1;
+	   	 num1=num2;
+	   	 num2=temp;
+	   	 System.out.print(num1+" "+num2);
 	   }
 	   else{
 	   	System.out.print("oops!enter  have must enter the below 100000 only");
