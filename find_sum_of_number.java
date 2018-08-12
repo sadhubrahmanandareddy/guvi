465ce3a51ebbf0b15151f8cea8c5364d1cf3581a
sadhubrahmanandareddy <41730749+sadhubrahmanandareddy@users.noreply.github.com>
2018-08-12 15:20:43 +0530

    Create find_sum_of_number.java

---
+++ find_sum_of_number.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n1,n2,sum;
+		n1=s.nextInt();
+		n2=s.nextInt();
+	           sum=n1+n2;
+	  System.out.println(sum);
+	}
+}
