3a7669fa9aa208645f8e52829940c82cece71717
sadhubrahmanandareddy <41730749+sadhubrahmanandareddy@users.noreply.github.com>
2018-08-12 21:29:52 +0530

    Create number_is_power_of_two_or_not.java

---
+++ number_is_power_of_two_or_not.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n,flag=0,p;
+		n=s.nextInt();
+		p=n;
+		while(p!=1)
+		{
+			if(p%2!=0)
+			{
+				flag=1;
+				break;
+			}
+			p=p/2;
+			
+		}
+		if(flag==0)
+		{
+			System.out.println("yes");
+			
+		}
+		else
+		System.out.println("no");
+	}
+	
+}
